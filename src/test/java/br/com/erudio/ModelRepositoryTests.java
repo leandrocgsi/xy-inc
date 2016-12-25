5c3864fcc37d334f5ff0146871287e35efb52e30
leandrocgsi <leandrocgsi@gmail.com>
2016-12-25 20:25:57 -0200

    Testing create endpoint

--- src/test/java/br/com/erudio/ModelRepositoryTests.java
+++ src/test/java/br/com/erudio/ModelRepositoryTests.java
@@ -2,23 +2,20 @@ package br.com.erudio;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.erudio.controller.entities.Model;
 import br.com.erudio.response.ResponseWrapper;
@@ -26,22 +23,48 @@ import br.com.erudio.response.ResponseWrapper;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebIntegrationTest
+@SuppressWarnings({ "rawtypes", "deprecation" })
 public class ModelRepositoryTests {
     
     final String BASE_PATH = "http://localhost:8080/api/model";
+    private RestTemplate restTemplate;
     
     @Before
     public void setUp() throws Exception {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseWrapper response = restTemplate.getForObject(BASE_PATH, ResponseWrapper.class);
-
-        restTemplate.delete(BASE_PATH + "/products");
+        restTemplate = new RestTemplate();
     }
     
     @Test
+    @SuppressWarnings("unchecked")
     public void testCreateModel() throws JsonProcessingException{
-        RestTemplate restTemplate = new RestTemplate();
-      
+        restTemplate.delete(BASE_PATH + "/products");
+
+        Model model = new Model();
+        Map<String, String> fields = new HashMap<String, String>();
+
+        fields.put("name", "string");
+        fields.put("description", "text");
+        fields.put("price", "decimal");
+        fields.put("category", "string");
+        model.setFields(fields);
+        model.setName("products");
+
+        //Insert a new model 
+        ResponseWrapper<Model> response = restTemplate.postForObject(BASE_PATH, model, ResponseWrapper.class);
+        assertNotNull(response);
+        Assert.assertEquals(HttpStatus.CREATED, response.getStatus());
+        
+        //Insert a repeated model 
+        ResponseWrapper<Model> response2 = restTemplate.postForObject(BASE_PATH, model, ResponseWrapper.class);
+        assertNotNull(response2);
+        Assert.assertEquals(HttpStatus.CONFLICT, response2.getStatus());
+    }
+    
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testUpdateModel() throws JsonProcessingException{
+        restTemplate.delete(BASE_PATH + "/products");
+        
         Model model = new Model();
         Map<String, String> fields = new HashMap<String, String>();
         
@@ -51,10 +74,23 @@ public class ModelRepositoryTests {
         fields.put("category", "string");
         model.setFields(fields);
         model.setName("products");
-      
-      ResponseEntity<ResponseWrapper<Model>> response = restTemplate.postForEntity(BASE_PATH, model, ResponseWrapper.class, Collections.EMPTY_MAP);
-
-      assertNotNull(response);
+        
+        //Insert a new model 
+        ResponseWrapper<Model> response = restTemplate.postForObject(BASE_PATH, model, ResponseWrapper.class);
+        assertNotNull(response);
+        Assert.assertEquals(HttpStatus.CREATED, response.getStatus());
+        
+        //Insert a repeated model 
+        ResponseWrapper<Model> response2 = restTemplate.postForObject(BASE_PATH, model, ResponseWrapper.class);
+        assertNotNull(response2);
+        Assert.assertEquals(HttpStatus.CONFLICT, response2.getStatus());
+    }
+    
+    @Test
+    public void testFindAll() throws JsonProcessingException{
+        ResponseWrapper response = restTemplate.getForObject(BASE_PATH, ResponseWrapper.class);
+        assertNotNull(response);
+        Assert.assertEquals(HttpStatus.OK, response.getStatus());
     }
 
 }
