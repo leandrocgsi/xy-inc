39c23f17499d1052d085962db48c18892bf8ff1c
leandrocgsi <leandrocgsi@gmail.com>
2016-12-25 16:55:11 -0200

    Starting tests implementations

--- src/test/java/br/com/erudio/builders/Response.java
+++
@@ -1,9 +0,0 @@
-package br.com.erudio.builders;
-
-public interface Response {
-	public static final String OK = "\"status\":\"OK\"";
-	public static final String CREATED = "\"status\":\"CREATED\"";
-	public static final String CONFLICT = "\"status\":\"CONFLICT\"";
-	public static final String NOT_FOUND = "\"status\":\"NOT_FOUND\"";
-	public static final String INTERNAL_SERVER_ERROR = "\"status\":\"INTERNAL_SERVER_ERROR\"";
-}
